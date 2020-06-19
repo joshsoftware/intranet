b00793c9bd58c262359e39345a0d3b2e43a0b499
Abhishek Sapate <sapate.abhishek1998@gmail.com>
2020-06-19 15:11:10 +0530

    changes in recipients

--- app/mailers/timesheet_remainder_mailer.rb
+++ app/mailers/timesheet_remainder_mailer.rb
@@ -8,8 +8,8 @@ class TimesheetRemainderMailer < ActionMailer::Base
     @text = text
     managers_emails = @user.get_managers_emails_for_timesheet
     all_receivers = managers_emails + DEFAULT_TIMESHEET_MANAGERS + ['hr@joshsoftware.com']
-    receivers = pending_more_than_threshold ? all_receivers : [managers_emails.first, 'hr@joshsoftware.com']
-    mail(subject: 'Timesheet Reminder', to: user.email, cc: receivers)
+    receivers = pending_more_than_threshold ? all_receivers : ['hr@joshsoftware.com']
+    mail(subject: 'Timesheet Reminder', to: user.email, cc: receivers )
   end
 
   def user_timesheet_for_diffrent_project(user, timesheets)
