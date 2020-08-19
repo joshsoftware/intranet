8a2c09ddbff3f0f5889046f2c9be297ed83a7970
Abhishek Sapate <sapate.abhishek1998@gmail.com>
2020-08-19 16:02:02 +0530

    Added testcases for api triggers

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -21,6 +21,59 @@ describe Repository do
     expect(repository.errors.full_messages).to eq(["Project can't be blank"])
   end
 
+  context 'Trigger - should call code monitor service' do
+    it 'when Repository is added' do
+      project = FactoryGirl.create(:project)
+      repository = FactoryGirl.build(:repository, project: project)
+      params = {
+        event_type:         'Repository Added',
+        repository_id:      repository.id,
+        repository_url:     repository.url,
+        project_id:         repository.project.id,
+        repository_details: repository.as_json({
+          only: [
+            :name, :host, :code_climate_id, :maintainability_badge,
+            :test_coverage_badge, :visibility, :rollbar_access_token
+          ]
+        })
+      }
+      uri = URI('http://localhost')
+      uri.query = URI.encode_www_form(params)
+      stub_request(:get, uri).
+        with(
+          headers: {
+            'Accept'=>'*/*',
+            'User-Agent'=>'Ruby'
+          }).
+        to_return(status: 200, body: "", headers: {})
+      expect(repository.new_record?).to eq(true)
+      repository.save
+      expect(repository).to be_valid
+    end
+
+    it 'when Repository is removed' do
+      project = FactoryGirl.create(:project)
+      repository = FactoryGirl.create(:repository, project: project)
+      params = {
+        event_type:         'Repository Removed',
+        repository_id:      repository.id,
+        repository_url:     repository.url,
+        project_id:         repository.project.id
+      }
+      uri = URI('http://localhost')
+      uri.query = URI.encode_www_form(params)
+      stub_request(:get, uri).
+        with(
+          headers: {
+            'Accept'=>'*/*',
+            'User-Agent'=>'Ruby'
+          }).
+        to_return(status: 200, body: "", headers: {})
+      repository.destroy
+      expect(Repository.count).to eq 0
+    end
+  end
+
   it 'should not create a Repository with invalid host' do
     project = FactoryGirl.create(:project)
     repository = FactoryGirl.build(:repository, project: project)
