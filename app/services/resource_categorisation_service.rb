5dc9a172b846011825deccee8b3475c6ee3da680
Tejas Zambre <tzambre681@gmail.com>
2020-11-24 18:44:47 +0530

    Limit technical skills to max 3 - Added validation on technical skills in public profile to have maximum 3 length - Change caption of Technical Skills to Core Technical Skills - Updated values of Language, Framework and Other global constants as per requirements - Modified the testcases for the above - Added rake task move other skills which moves the skills other than skills in core technical skill set - Added rake task max three technical skills to keep only 3 skills in technical skills field and move all other skills in skills field

--- app/services/resource_categorisation_service.rb
+++ app/services/resource_categorisation_service.rb
@@ -18,8 +18,10 @@ class ResourceCategorisationService
       'UI/UX Designer',
       'Senior UI/UX Designer',
     ]
-    devops_project = Project.where(name: 'DevOps Work').first
-    devops_engg = UserProject.where(project_id: devops_project.id, active: true).pluck(:user_id)
+    devops_engg = UserProject.where(
+      project_id: @devops_project.id,
+      active: true
+    ).pluck(:user_id)
     exclude_designations = [
       'Assistant Vice President - Sales',
       'Business Development Executive',
@@ -42,20 +44,32 @@ class ResourceCategorisationService
         technical_skills = user.public_profile.try('technical_skills').slice(0,3)
 
         next devops_ui_ux_resource.append(
-          add_resource_record(user, technical_skills, project_names).merge(non_billable: 160, total_allocation: 160)
-        ) if ui_ux_designation.include?(user.designation.try(:name)) || devops_engg.include?(user.id)
+          add_resource_record(user).merge(
+            non_billable: 160,
+            total_allocation: 160,
+            technical_skills: technical_skills,
+            projects: project_names
+          )
+        ) if ui_ux_designation.include?(user.designation.try(:name)) ||
+             devops_engg.include?(user.id)
 
         next bench_resource.append(
-          add_resource_record(user, technical_skills, project_names).merge(bench: 160)
+          add_resource_record(user).merge(
+            bench: 160,
+            technical_skills: technical_skills,
+            projects: project_names
+          )
         ) if total_allocation == 0
 
-        @report[:resource_report] << add_resource_record(user, technical_skills, project_names).merge(
+        @report[:resource_report] << add_resource_record(user).merge(
           total_allocation: total_allocation,
           billable: billable_allocation,
           non_billable: non_billable_allocation,
           investment: investment_allocation,
-          bench: bench_allocation
-        )
+          bench: bench_allocation,
+          technical_skills: technical_skills,
+          projects: project_names
+      )
       end
     end
 
@@ -107,14 +121,14 @@ class ResourceCategorisationService
       allocation = user_project.allocation
       next if allocation == 0
       allocation_type = 'non_billable' if user_project.billable == false
-      @report[:project_wise_resource_report] << add_project_wise_resource_record(user).merge(
+      @report[:project_wise_resource_report] << add_project_wise_record(user).merge(
         "#{allocation_type}": allocation,
         project: user_project.project.name
       )
     end
   end
 
-  def add_resource_record(user, technical_skills, project_names)
+  def add_resource_record(user)
     {
       name: user.name,
       location: user.location,
@@ -124,12 +138,12 @@ class ResourceCategorisationService
       non_billable: 0,
       investment: 0,
       bench: 0,
-      technical_skills: technical_skills,
-      projects: project_names
+      technical_skills: [],
+      projects: []
     }
   end
 
-  def add_project_wise_resource_record(user)
+  def add_project_wise_record(user)
     {
       name: user.name,
       location: user.location,
@@ -146,6 +160,8 @@ class ResourceCategorisationService
       resource_report: [],
       project_wise_resource_report: []
     }
+
+    @devops_project = Project.where(name: 'DevOps Work').first
 
     @billable_projects = Project.where(
       :type_of_project.in => ['T&M', 'Fixbid'],
