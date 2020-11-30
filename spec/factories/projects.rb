c909988187c7042deadf3fc15f85f711ff5eb468
Tejas Zambre <tzambre681@gmail.com>
2020-11-30 13:57:34 +0530

    Removed Code climate related fields from project model - Removed project benchmark column from project list table - Removed project badges from dashboard, user show template and project show template as well - Modified the factory of project for the same

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
   factory :project do
     name { Faker::App.name }
-    code_climate_id { Faker::Bank.swift_bic }
-    code_climate_snippet { Faker::Book.title }
     code { Faker::Lorem.characters(10) }
     rails_version { Faker::App.semantic_version }
     ruby_version { Faker::App.semantic_version }
