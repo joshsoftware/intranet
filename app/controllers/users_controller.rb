9189c09de59ed7b826bd46f4843aec4fc3c0c198
Ashish <aashishpande944@gmail.com>
2020-12-13 16:53:34 +0530

    Skip project not assigned mail - Added field as skip_unassigned_project_ts_mail in employee_details model default true - Updated query for project not assigned mail - Added toggle switch for above field in employee details template - Modified test-cases - Removed old validation and method to assign emp_ids

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -144,8 +144,8 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:status, :role, :visible_on_website, :website_sequence_number,
-      :allow_backdated_timesheet_entry, employee_detail_attributes: [:id, :employee_id, :location,
-      :date_of_relieving, :designation, :description, :is_billable, :designation_track, :notification_emails => [] ],
+      :allow_backdated_timesheet_entry, employee_detail_attributes: [:id, :employee_id, :location, :date_of_relieving,
+      :designation, :description, :is_billable, :skip_unassigned_project_ts_mail, :designation_track, :notification_emails => [] ],
       attachments_attributes: [:id, :name, :document, :_destroy]
     )
   end
@@ -189,9 +189,11 @@ class UsersController < ApplicationController
 
   def get_bonusly_messages
     bonusly = Api::Bonusly.new(BONUSLY_TOKEN)
-    bonusly.bonusly_messages(start_time: Date.today.strftime('%B+1st'),
-                             end_time:   Date.today.end_of_month.strftime('%B+%dst'),
-                             user_email: @user.email)
+    bonusly.bonusly_messages(
+      start_time: Date.today.strftime('%B+1st'),
+      end_time:   Date.today.end_of_month.strftime('%B+%dst'),
+      user_email: @user.email
+    )
   end
 
   def get_github_feed
--- app/models/employee_detail.rb
+++ app/models/employee_detail.rb
@@ -11,13 +11,13 @@ class EmployeeDetail
   field :available_leaves, type: Integer, default: 0
   field :description
   field :is_billable, type: Boolean, default: false
+  field :skip_unassigned_project_ts_mail, type: Boolean, default: false
   field :designation_track, type: String, default: DESIGNATION_TRACKS.first
   field :location
 
   belongs_to :designation
 
   validates :employee_id, uniqueness: true
-  #validates :employee_id, numericality: {greater_than_or_equal_to: 9000}, if: :assign_new_usa_id?
   #validates :designation_track, presence: true
   validates :available_leaves, numericality: {greater_than_or_equal_to: 0}
   after_update :delete_team_cache, if: Proc.new{ updated_at_changed? }
@@ -44,11 +44,5 @@ class EmployeeDetail
   def add_rejected_leave(number_of_days)
     remaining_leaves = available_leaves + number_of_days
     self.update_attribute(:available_leaves, remaining_leaves)
-  end
-
-  def assign_new_usa_id?
-    # For Sachin's record we are keeping his employee ID as it is
-    # For all other USA employees ID must be above 9000
-    employee_id.nil? and location == "Plano" ? true : false
   end
 end
