9935832d949905cf3301b9361fc8e4162b70f61b
Rohit Nale <rohitnale738@gmail.com>
2021-05-21 10:42:09 +0530

    RC report changes: - Added total count of each column billable, non_billable, investment, shared, bench. - Remove the empty row. - Added one more rule in shared_resource column. - Update test cases.

--- spec/services/resource_categorisation_service_spec.rb
+++ spec/services/resource_categorisation_service_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe ResourceCategorisationService do
       )
       @investment_project = FactoryGirl.create(
         :project,
-        name: 'Hunger Terminal',
+        name: 'OnCoT',
         type_of_project: 'Investment'
       )
 
@@ -105,42 +105,45 @@ RSpec.describe ResourceCategorisationService do
       technical_skills_one = @emp_one.public_profile.try('technical_skills').slice(0,3)
       technical_skills_two = @emp_two.public_profile.try('technical_skills').slice(0,3)
       technical_skills_three = @emp_three.public_profile.try('technical_skills').slice(0,3)
-      report = [
-        {
-          id: @emp_one.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
-          name: @emp_one.name,
-          location: @emp_one.location,
-          designation: @emp_one.designation.try(:name),
-          level: @service.get_level(@emp_one),
-          total_allocation: 160,
-          billable: @user_project_one.allocation,
-          non_billable: 0,
-          investment: 0,
-          shared: 0,
-          bench: 80,
-          technical_skills: technical_skills_one,
-          exp_in_months: @emp_one.experience_as_of_today,
-          projects: @service.get_project_names(@emp_one)
-        },
-        {
-          id: @emp_two.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
-          name: @emp_two.name,
-          location: @emp_two.location,
-          designation: @emp_two.designation.try(:name),
-          level: @service.get_level(@emp_two),
-          total_allocation: 160,
-          billable: 0,
-          non_billable: @user_project_two.allocation,
-          investment: 0,
-          shared: 0,
-          bench: 60,
-          exp_in_months: @emp_two.experience_as_of_today,
-          technical_skills: technical_skills_two,
-          projects: @service.get_project_names(@emp_two)
+      report = {
+        resource_report: {
+          records: [
+            {
+              id: @emp_one.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
+              name: @emp_one.name,
+              location: @emp_one.location,
+              designation: @emp_one.designation.try(:name),
+              level: @service.get_level(@emp_one),
+              total_allocation: 160,
+              billable: @user_project_one.allocation,
+              non_billable: 0,
+              investment: 0,
+              shared: 0,
+              bench: 80,
+              technical_skills: technical_skills_one,
+              exp_in_months: @emp_one.experience_as_of_today,
+              projects: @service.get_project_names(@emp_one)
+            },
+            {
+              id: @emp_two.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
+              name: @emp_two.name,
+              location: @emp_two.location,
+              designation: @emp_two.designation.try(:name),
+              level: @service.get_level(@emp_two),
+              total_allocation: 160,
+              billable: 0,
+              non_billable: @user_project_two.allocation,
+              investment: 0,
+              shared: 0,
+              bench: 60,
+              exp_in_months: @emp_two.experience_as_of_today,
+              technical_skills: technical_skills_two,
+              projects: @service.get_project_names(@emp_two)
+            }
+          ]
         }
-      ]
-      report = report.sort_by { |k| k[:name] }
-      report.append({blank_row: true})
+      }
+      report = report[:resource_report][:records].sort_by { |k,v| k[:name] }
       report << {
         id: @emp_three.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
         name: @emp_three.name,
@@ -157,79 +160,95 @@ RSpec.describe ResourceCategorisationService do
         technical_skills: technical_skills_three,
         projects: @service.get_project_names(@emp_three)
       }
-      report.append({blank_row: true})
+
+      total_count = {
+        :billable=>[0, 2, 1], :non_billable=>[1, 1, 1],
+        :investment=>[0, 0, 3], :shared=>[0, 0, 3], :bench=>[0, 2, 1]
+      }
 
       @service.load_projects
       response = @service.generate_resource_report
-      expect(response[:resource_report]).to eq(report)
+      expect(response[:resource_report][:records]).to eq(report)
+      expect(response[:resource_report][:total_count]).to eq(total_count)
     end
 
     it 'should generate project wise resource report as expected' do
-      report = [
-        {
-          code: @active_project.code,
-          project: @active_project.name,
-          type_of_project: @active_project.type_of_project,
-          billing_frequency: @active_project.billing_frequency,
-          emp_id: @emp_one.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
-          name: @emp_one.name,
-          location: @emp_one.location,
-          designation: @emp_one.designation.try(:name),
-          billable: @user_project_one.allocation,
-          non_billable: 0,
-          investment: 0,
-          shared: 0,
-          bench: 80
-        },
-        {
-          code: @devops_project.code,
-          project: @devops_project.name,
-          type_of_project: @devops_project.type_of_project,
-          billing_frequency: @devops_project.billing_frequency,
-          emp_id: @emp_three.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
-          name: @emp_three.name,
-          location: @emp_three.location,
-          designation: @emp_three.designation.try(:name),
-          billable: 0,
-          non_billable: @user_project_three.allocation,
-          investment: 0,
-          shared: 0,
-          bench: 0
-        },
-        {
-          code: @active_project_two.code,
-          project: @active_project_two.name,
-          type_of_project: @active_project_two.type_of_project,
-          billing_frequency: @active_project_two.billing_frequency,
-          emp_id: @emp_two.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
-          name: @emp_two.name,
-          location: @emp_two.location,
-          designation: @emp_two.designation.try(:name),
-          billable: 0,
-          non_billable: @user_project_two.allocation,
-          investment: 0,
-          shared: 0,
-          bench: 60
-        },
-        {
-          code: @active_project_two.code,
-          project: @active_project_two.name,
-          type_of_project: @active_project_two.type_of_project,
-          billing_frequency: @active_project_two.billing_frequency,
-          emp_id: @emp_three.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
-          name: @emp_three.name,
-          location: @emp_three.location,
-          designation: @emp_three.designation.try(:name),
-          billable: @user_project_four.allocation,
-          non_billable: 0,
-          investment: 0,
-          shared: 0,
-          bench: 0
+      report = {
+        project_wise_resource_report: {
+          records: [
+            {
+              code: @active_project.code,
+              project: @active_project.name,
+              type_of_project: @active_project.type_of_project,
+              billing_frequency: @active_project.billing_frequency,
+              emp_id: @emp_one.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
+              name: @emp_one.name,
+              location: @emp_one.location,
+              designation: @emp_one.designation.try(:name),
+              billable: @user_project_one.allocation,
+              non_billable: 0,
+              investment: 0,
+              shared: 0,
+              bench: 80
+            },
+            {
+              code: @devops_project.code,
+              project: @devops_project.name,
+              type_of_project: @devops_project.type_of_project,
+              billing_frequency: @devops_project.billing_frequency,
+              emp_id: @emp_three.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
+              name: @emp_three.name,
+              location: @emp_three.location,
+              designation: @emp_three.designation.try(:name),
+              billable: 0,
+              non_billable: @user_project_three.allocation,
+              investment: 0,
+              shared: 0,
+              bench: 0
+            },
+            {
+              code: @active_project_two.code,
+              project: @active_project_two.name,
+              type_of_project: @active_project_two.type_of_project,
+              billing_frequency: @active_project_two.billing_frequency,
+              emp_id: @emp_two.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
+              name: @emp_two.name,
+              location: @emp_two.location,
+              designation: @emp_two.designation.try(:name),
+              billable: 0,
+              non_billable: @user_project_two.allocation,
+              investment: 0,
+              shared: 0,
+              bench: 60
+            },
+            {
+              code: @active_project_two.code,
+              project: @active_project_two.name,
+              type_of_project: @active_project_two.type_of_project,
+              billing_frequency: @active_project_two.billing_frequency,
+              emp_id: @emp_three.employee_detail.try(:employee_id).try(:rjust, 3, '0'),
+              name: @emp_three.name,
+              location: @emp_three.location,
+              designation: @emp_three.designation.try(:name),
+              billable: @user_project_four.allocation,
+              non_billable: 0,
+              investment: 0,
+              shared: 0,
+              bench: 0
+            }
+          ]
         }
-      ]
-      report = report.sort_by { |k| k[:project] }
+      }
+
+      total_count = {
+        :billable => [0, 2, 2], :non_billable => [1, 1, 2],
+        :investment => [0, 0, 4], :shared=>[0, 0, 4], :bench => [0, 2, 2]
+      }
+
+      report = report[:project_wise_resource_report][:records].sort_by { |k,v| k[:project] }
       response = @service.generate_resource_report
-      expect(response[:project_wise_resource_report]).to eq(report)
+      expect(response[:project_wise_resource_report][:records]).to eq(report)
+      expect(response[:project_wise_resource_report][:total_count]).to eq(total_count)
     end
 
     it 'should send mail' do
